d611f313969f49166a20da7ef69924e974ea1f1a
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-17 13:58:51 +0100

    refactor: changes to app.js and imports for BrowserRouter in index.js file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import {Switch, Route} from "react-router-dom";
+import Home from './components/Home';
+import Search from './components/search';
 import BookShelf from './components/BookShelf';
 import FavoriteBook from './components/FavoriteBook';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
+   
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          
-            
-        ) : (
-         
-        )}
-      </div>
-    )
-  }
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Search" component={Search} />
+
+      </Switch>
+     </div>
+    );
 }
+}
+
 
 export default BooksApp;
